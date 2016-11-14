210507516d00ee83c5dd41bd30a6ca9ad91afbce
dmarkov00 <dmarkov23@gmail.com>
2016-11-14 02:52:27 +0100

    added some authentication and validation improvements

--- server/config/passport.js
+++ server/config/passport.js
@@ -4,17 +4,18 @@ const User = require('mongoose').model('User')
 
 module.exports = () => {
   passport.use(new LocalPassport({
-    usernameField: 'username',
+    usernameField: 'email',
     passportField: 'passport'
   },
 
-      (username, password, done) => {
-        User.findOne({ username: username }).then(user => {
-          if (!user) return done(null, false)
-          if (!user.authenticate(password)) return done(null, false)
-          return done(null, user)
-        })
-      }))
+    (username, password, done) => {
+      User.findOne({ usern: username }).then(user => {
+        console.log(user)
+        if (!user) return done(null, false)
+        if (!user.authenticate(password)) return done(null, false)
+        return done(null, user)
+      })
+    }))
 
   passport.serializeUser((user, done) => {
     if (user) return done(null, user._id)
@@ -27,4 +28,3 @@ module.exports = () => {
     })
   })
 }
-
--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -1,5 +1,7 @@
 let encryption = require('../utilities/encryption')
 let User = require('mongoose').model('User')
+const emailRegularExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+let errorMessages = []
 module.exports = {
   register: (req, res) => {
     res.render('users/register')
@@ -7,17 +9,29 @@ module.exports = {
   create: (req, res) => {
     let user = req.body
 
-    if (user.password !== user.confirmPassword) {
-      user.globalError = 'Passwords do not match!'
-      res.render('users/register', user)
-    } else {
+    // validating user input from the form
+    if (user.password === '' || user.confirmPassword === '' || user.username === '' || user.email === '') {
+      errorMessages.push('All fields are required.')
+    } if (!emailRegularExpression.test(user.email)) {
+      errorMessages.push("That doesn't appear to be a valid email address.")
+    } if (user.password !== user.confirmPassword) {
+      errorMessages.push('Passwords do not match.')
+    } if (user.password < 4) {
+      // TODO: add better validation for password(regex for strong password)
+      errorMessages.push('Password too short.')
+    } if (user.username < 4) {
+      errorMessages.push('Username too short.')
+      user.errorMessages = errorMessages
+    } if (errorMessages.length === 0) {
       user.salt = encryption.generateSalt()
       user.hashedPass = encryption.generateHashedPassword(user.salt, user.password)
 
       User.create(user).then(user => {
+        // if (err) errorMessages.push(err)
+
         req.logIn(user, (err, user) => {
           if (err) {
-            user.globalError = err
+            user.errorMessages = err
             res.render('users/register', user)
             return
           }
@@ -25,22 +39,24 @@ module.exports = {
           res.redirect('/')
         })
       })
+    } else {
+      return res.render('users/register', user)
     }
   },
   login: (req, res) => {
     res.render('users/login')
   },
-  authenticate: (req, res) => {
+  authenticate: (req, res) => { // called on user login request
     let inputUser = req.body
 
     User.findOne({ email: inputUser.email })
       .then(user => {
-        if (!user.authenticate(inputUser.password)) {
-          res.render('users/login', {globalError: 'Invalid username or password'})
+        if (!user.authenticate(inputUser.password)) { // the method authenticate comes from the user model
+          res.render('users/login', {errorMessage: 'Invalid username or password.'})
         } else {
-          req.logIn(user, (err, user) => {
+          req.logIn(user, (err, user) => { // method comes from passport
             if (err) {
-              res.render('users/login', {globalError: 'ops 500 passport ima bug'})
+              res.render('users/login', {globalError: 'Internal server error'})
               return
             }
             res.redirect('/')
